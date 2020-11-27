219a55f73792595b9c0508d63f472e856862da4e
Kazunari-h <kazunario456@gmail.com>
2020-11-27 14:45:32 +0900

    投稿画面の追加

---
+++ src/pages/post.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import View from "../components/View"
+import Status from "../components/Status"
+
+const Post = () => (
+  <Layout>
+    <Status />
+    <View title="投稿画面">
+      <p>テスト</p>
+    </View>
+  </Layout>
+)
+
+export default Post
